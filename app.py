a90933ec84b05cb4a12f558dbaff924e19cbf2cc
simdax <simoncornaz@gmail.com>
2016-09-02 19:31:22 +0200

    rajout de boutons

--- app.py
+++ app.py
@@ -1,34 +1,76 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+import cv2
+from PyQt4 import QtGui, QtCore
+
 
 # communication avec supercollider
 # oscPort=57121
 import supercollider
 from osc import *
 
-import cv2
+class Capture():
 
-def tbCallback(n):
-    cap.set(1,n)
-    bob()
 
-cap = cv2.VideoCapture("/home/simdax/Vidéos/Nirvana - In Bloom.mp4")
-# 7 prop = nbFrames
-nbFrames= cap.get(7)
-
-#open win
-cv2.namedWindow('frame')
-cv2.createTrackbar("test", 'frame', 0, int(nbFrames), tbCallback)
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',gray)
     
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        kill()
-        break
-    
-cap.release()
-cv2.destroyWindow('frame')
+    def __init__(self):
+        self.c = cv2.VideoCapture("/home/simdax/Vidéos/Nirvana - In Bloom.mp4")
+        self.go = False
+
+        #helper
+    def tbCallback(self, n):
+        self.c.set(1,n)
+        
+    def togglePause(self):
+        if self.go == False:
+            self.go = True
+            print "plus pause"
+        else:
+            self.go=False
+            print "pause"
+            
+    def startCapture(self):
+        print "pressed start"
+        self.togglePause()
+        cv2.namedWindow("Capture")
+        nbFrames= self.c.get(7)
+        cv2.createTrackbar("test", "Capture", cv2.getTrackbarPos("test", "Capture"), int(nbFrames), self.tbCallback)
+        while(self.go):
+            ret, frame = self.c.read()
+            cv2.imshow("Capture", frame)
+
+            cv2.waitKey(25)
+
+    def quitCapture(self):
+        print "pressed Quit"
+        self.go=False
+        cv2.destroyAllWindows()
+        self.c.release()
+        QtCore.QCoreApplication.quit()
+
+
+class Window(QtGui.QWidget):
+    def __init__(self):
+
+        QtGui.QWidget.__init__(self)
+        self.setWindowTitle('Control Panel')
+
+        self.capture = Capture()
+
+        self.start_button = QtGui.QPushButton('Start/Pause',self)
+        self.start_button.clicked.connect(self.capture.startCapture)
+
+        self.quit_button = QtGui.QPushButton('Quit',self)
+        self.quit_button.clicked.connect(self.capture.quitCapture)
+
+        vbox = QtGui.QVBoxLayout(self)
+        vbox.addWidget(self.start_button)
+        vbox.addWidget(self.quit_button)
+
+        self.setLayout(vbox)
+        self.setGeometry(100,100,200,200)
+        self.show()
+
+#if __name__ == '__main__' :
+import sys
+app = QtGui.QApplication(sys.argv)
+window = Window()
+sys.exit(app.exec_())
